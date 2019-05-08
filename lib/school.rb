e987c7f1485c04689c49a75c6ae550fc11a249ec
Janice Pena <jipena94@gmail.com>
2019-05-08 02:16:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, att)
+    roster[att] = [] if !roster.keys.include?(att)
+    roster[att] << student
+  end
+  
+  def grade(letter)
+    roster[letter]
+  end
+  
+  def sort
+    sorted = {}
+    roster.each {|k, v| sorted[k] = v.sort}
+    sorted
+    end
+end
+
+   
+    
